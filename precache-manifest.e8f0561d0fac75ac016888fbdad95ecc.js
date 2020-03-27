c341fa89998c2d7993efe337c400c5ed11dcc9d9
WildPengu <7241679@wp.pl>
2020-03-27 12:54:21 +0100

    Updates

---
+++ precache-manifest.e8f0561d0fac75ac016888fbdad95ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4272d71f8410600fe7a0e519e38b0adf",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "8d294c47074c83a98dbc",
+    "url": "/nazwa/static/css/main.ed05b2db.chunk.css"
+  },
+  {
+    "revision": "945bacfe2fc9fff6b2a5",
+    "url": "/nazwa/static/js/2.a41c0621.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nazwa/static/js/2.a41c0621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d294c47074c83a98dbc",
+    "url": "/nazwa/static/js/main.dd42b556.chunk.js"
+  },
+  {
+    "revision": "7c2a9b202dee170467da",
+    "url": "/nazwa/static/js/runtime-main.3cd692f7.js"
+  }
+]);
